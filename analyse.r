df123c473dabbc15f2ee1a38974923476d49d33e
Sebastian Sauer <ssauer@posteo.de>
2026-04-10 17:38:23 +0200

    more stats integration

---
+++ analyse.r
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(easystats)
+library(ggpubr)
+library(knitr)
+library(gt)
+library(tinytable)
+library(modelsummary)
+
+mariokart <- read_csv("mariokart.csv")
+
+
+mariokart_no_outliers <-
+  mariokart |> 
+  filter(total_pr < 100)
+
+desk1 <-
+  mariokart |>
+  select(total_pr) |>
+  describe_distribution()
+
+
+lm_hyp1 <- 
+  lm(total_pr ~ cond + total_pr + start_pr, data = mariokart)
+
+
+lm1_modelsumm <- modelsummary(lm_hyp1)
